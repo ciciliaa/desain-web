8ed556f507e89717cad30f2a858b8bb33130ca65
Cicilia Marissa Laurina <71742584+ciciliaa@users.noreply.github.com>
2021-05-08 20:03:58 +0700

    Create isi.js

---
+++ modul8-Javascript/isi.js
@@ -0,0 +1,7 @@
+document.writeln("<center>");
+document.writeln("<hr width=600 color=tosca>");
+document.writeln("<h1>Belajar Javascript</h1>");
+document.writeln("<h2>PTIK UNS 2020</h2>");
+document.writeln("<h4>NIM K3520017</h4>");
+document.writeln("<hr width=600 color=black>");
+document.writeln("</center>");
